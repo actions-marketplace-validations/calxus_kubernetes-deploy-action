4743f463b897fc39eb1e424633dc12875fd60c99
Gordon Adam-Santiago <gtadam@protonmail.ch>
2020-12-17 09:17:36 +0000

    Adding the possibility of an upgrade

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@
 mkdir ~/.kube
 echo $1 | base64 -d > ~/.kube/config
 
-helm install $2 service
+DEPLOYED_SERVICE=$(helm list --output json | jq -r ".[] | select( .name == \"$2\" ) | .name")
+if [ $DEPLOYED_SERVICE = $2 ] ; then
+    helm upgrade $2 service
+else
+    helm install $2 service
+fi
